d1ee9cba08dc40fdc39c48a217b175a79c3292f4
Leandro Calvetti <lcalvetti1@gmail.com>
2023-06-10 14:52:05 -0300

    correccion de nombre de paquetes

--- index.php
+++ index.php
@@ -1,10 +1,7 @@
 <?php
 
+use Leandro\app\libs\App;
+
+require_once 'vendor/autoload.php';
 //echo "llama al main si dd";
-$c              = $_GET['c'] ?? "index";
-$m              = $_GET['m'] ?? "index";
-$con            = ucfirst($c) . "_Controller";
-$controllerPath = 'src/controlador/' . $con . ".php";
-require_once $controllerPath;
-$controller = new $con();
-$controller->{$m}();
+App::iniciar();
